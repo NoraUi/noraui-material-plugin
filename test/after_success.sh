df67bfa437188a3a1784df07ea00af301e3d00ac
sgrillon14 <sgrillon@hotmail.fr>
2020-06-09 16:13:36 +0200

    initial commit

---
+++ test/after_success.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "TRAVIS_REPO_SLUG is $TRAVIS_REPO_SLUG"
+echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
+echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
+    
+if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/noraui-material-plugin' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    cd project
+    mvn_version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.5.0:exec)
+    echo "Maven version is $mvn_version"
+    if [[ $mvn_version == *"-SNAPSHOT" ]]; then
+        echo "******** Starting deploy snapshot"
+        mvn clean deploy -Psnapshot --settings test/mvnsettings.xml -DskipTests=true
+        exit $?
+    fi
+fi
