0ab5b42c0e0dcbf09c46bef7c7b88efc549892dd
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-06-10 23:07:40 +0200

    fix clean package

--- test/run.sh
+++ test/run.sh
@@ -5,7 +5,7 @@ echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
 
 if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/noraui-material-plugin' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-exit	    mvn -e -U clean package --settings test/mvnsettings.xml
+    mvn -e -U clean package --settings test/mvnsettings.xml
 fi
 echo "***************   END build of noraui-material-plugin  ***************"
 exit 0
