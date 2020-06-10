abcaeb9c0b75572cb5faf094901a208d16a662a6
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-06-10 23:30:02 +0200

    add good exit code

--- test/run.sh
+++ test/run.sh
@@ -7,7 +7,9 @@ echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
 
 if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/noraui-material-plugin' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    echo "******** Starting deploy snapshot"
     mvn -e -U clean package --settings test/mvnsettings.xml
+    exit $?
 fi
 echo "***************   END build of noraui-material-plugin  ***************"
 exit 0
