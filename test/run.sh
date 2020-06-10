2ce1d64c0be7b92072e3dd3c1906ec248136e552
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-06-10 22:50:17 +0200

    clean package

--- test/run.sh
+++ test/run.sh
@@ -1,25 +1,11 @@
-cd $(dirname $0)
 cd ..
-
-curl -Lo travis_after_all.py https://raw.github.com/dmakhno/travis_after_all/master/travis_after_all.py
-python travis_after_all.py https://api.travis-ci.org
-export $(cat .to_export_back)
 
 echo "TRAVIS_REPO_SLUG is $TRAVIS_REPO_SLUG"
 echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
-echo "AUTHOR_NAME is $AUTHOR_NAME"
-echo "BUILD_LEADER is $BUILD_LEADER"
-echo "BUILD_AGGREGATE_STATUS is $BUILD_AGGREGATE_STATUS"
 
-if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/noraui-material-plugin' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$AUTHOR_NAME" != 'Travis-CI' ] && [ "$BUILD_LEADER" == 'YES' ] && [ "$BUILD_AGGREGATE_STATUS" == 'others_succeeded' ]; then
-    mvn_version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.5.0:exec)
-    echo "Maven version is $mvn_version"
-    if [[ $mvn_version == *"-SNAPSHOT" ]]; then
-        echo "******** Starting deploy snapshot"
-        mvn clean deploy -Psnapshot --settings test/mvnsettings.xml -DskipTests=true
-        exit $?
-    fi
+if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/noraui-material-plugin' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+exit	    mvn -e -U clean package --settings test/mvnsettings.xml
 fi
 echo "***************   END build of noraui-material-plugin  ***************"
 exit 0
