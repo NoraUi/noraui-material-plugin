1ffd58a565cd9b8abe06d71448963a7a7c6ddfaf
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-06-14 21:21:36 +0200

    fix caller repository

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 cd $(dirname $0)
 cd ..
-if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/NoraUi' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/noraui-material-plugin' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn -e -U clean package --settings test/mvnsettings.xml
 fi
 echo "***************   END build of noraui-material-plugin  ***************"
